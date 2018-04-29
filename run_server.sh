a90238f34be81c130b7bc08a8b674b8f9421b301
silentcricket <silentcricket@comcast.net>
2018-04-28 21:17:29 -0400

    initial commit

---
+++ run_server.sh
@@ -0,0 +1,10 @@
+#if [ -z "$1" ]
+#  then
+#    echo "Usage: ./run_server.sh <Server Name> <Module Name>"
+#    exit 1
+#fi
+
+SERVERNAME='Nymri Ascension Test 1'
+MODULE=JoeTestModule
+
+docker run -dit -p 5121:5121/udp -e NWN_PORT=5121 --name Nymri -v $(pwd):/nwn/home -e NWN_MODULE=$MODULE -e NWN_PUBLICSERVER=1 -e NWN_PLAYERPASSWORD=rocktroll -e NWN_SERVERNAME="$SERVERNAME" -e NWN_PAUSEANDPLAY=0 -e NWN_PVP=0 beamdog/nwserver
---
+++ stop_server.sh
@@ -0,0 +1,3 @@
+docker stop Nymri
+
+docker rm Nymri
