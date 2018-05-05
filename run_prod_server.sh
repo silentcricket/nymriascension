2bf1d82ef5f9fc43b3b6489b325ee8e6ec44d3b6
silentcricket <silentcricket@comcast.net>
2018-05-05 16:55:29 -0400

    update run scripts

--- run_prod_server.sh
+++ run_prod_server.sh
@@ -7,4 +7,4 @@
 SERVERNAME='Nymri Ascension'
 MODULE='Nymri Ascension'
 
-docker run -dit -p 5121:5121/udp -e NWN_PORT=5121 --name Nymri -v $(pwd):/nwn/home -e NWN_MODULE=$MODULE -e NWN_PUBLICSERVER=1 -e NWN_PLAYERPASSWORD=rocktroll -e NWN_SERVERNAME="$SERVERNAME" -e NWN_PAUSEANDPLAY=0 -e NWN_PVP=0 beamdog/nwserver
+docker run -dit -p 5121:5121/udp -e NWN_PORT=5121 --name Nymri -v $(pwd):/nwn/home -e NWN_MODULE="$MODULE" -e NWN_PUBLICSERVER=1 -e NWN_PLAYERPASSWORD=rocktroll -e NWN_SERVERNAME="$SERVERNAME" -e NWN_PAUSEANDPLAY=0 -e NWN_PVP=0 beamdog/nwserver
