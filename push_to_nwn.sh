d40caa762ead112bbcbbf41256d2ba7f23d5deae
silentcricket <silentcricket@comcast.net>
2018-05-05 16:48:16 -0400

    adding push_to_nwn.sh

---
+++ push_to_nwn.sh
@@ -0,0 +1,12 @@
+# This script will move files from this current location to the folder where nwn server is running. In this case is /nwn_server.
+
+
+
+echo "Copying over shell scripts to nwn_server folder"
+cp -rf /nymriascension/*.sh /nwn_server/.
+
+
+echo "Copying over modules"
+cp -rf /nyriascension/*.mod /nwn_server/.
+
+ 
